2b80f473e7cfe3010c80f915dbf19de885dc0ff2
alihusain01 <48800789+alihusain01@users.noreply.github.com>
2024-02-19 00:08:39 -0600

    progress on node connection

--- node.go
+++ node.go
@@ -36,6 +36,86 @@ func findLocalIP() string {
 
 }
 
+// Check if a connection to the node already exists
+func connectionExists(name, ip, port string) bool {
+	for _, node := range nodes {
+		if node.IP == ip && node.Port == port {
+			node.Name = name
+			return true
+		}
+	}
+	return false
+}
+
+func handleConfiguration() {
+
+	file, err := os.Open("config.txt")
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	defer file.Close()
+
+	fmt.Println("Successfully opened config file")
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan() // Skip the first line (number of nodes)
+
+	localIP := findLocalIP()
+	localParts := strings.Split(localIP, ":")
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, " ")
+
+		fmt.Println("Parts:", parts)
+
+		if len(parts) != 3 {
+			fmt.Println("Invalid line:", line)
+			continue
+		}
+
+		node := Node{
+			Name: parts[0],
+			IP:   parts[1],
+			Port: parts[2],
+		}
+
+		// Skip if the node is the current node
+		if node.IP == localParts[0] && node.Port == localParts[1] {
+			fmt.Println("Skipping self node", node.Name)
+			nodes = append(nodes, node)
+			continue
+		}
+
+		// Initiate a connection with the node if there isn't one already
+		if !connectionExists(node.Name, node.IP, node.Port) {
+			conn, err := net.Dial("tcp", node.IP+":"+node.Port)
+			if err != nil {
+				fmt.Println("Error connecting to node:", err)
+				continue // Use continue instead of return to try the next node
+			}
+
+			// COMMENTING THIS OUT FOR NOW
+			//defer conn.Close()
+
+			// Store the connection and save the node
+			node.Connection = conn
+			nodes = append(nodes, node)
+
+			fmt.Println("Successfully connected to node", node.Name)
+		} else {
+			fmt.Println("Connection already exists for node", node.Name)
+		}
+
+		fmt.Println("Successfully connected to node", node.Name)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+}
+
 func main() {
 
 	/*
@@ -43,7 +123,7 @@ func main() {
 	*/
 
 	arguments := os.Args
-	if len(arguments) < 1 {
+	if len(arguments) < 2 {
 		fmt.Println("Please provide port number")
 		return
 	}
@@ -58,68 +138,53 @@ func main() {
 	fmt.Println("Server listening on port", arguments[1])
 	defer l.Close()
 
-	/*
-		Handle configuration file
-	*/
+	configTrigger := make(chan bool)
+	connectionTrigger := make(chan bool)
 
-	file, err := os.Open("config.txt")
-	if err != nil {
-		fmt.Println("Error: ", err)
-		return
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	scanner.Scan() // Skip the first line (number of nodes)
-
-	localIP := findLocalIP()
-	localParts := strings.Split(localIP, ":")
-
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		parts := strings.Split(line, " ")
-
-		if len(parts) != 3 {
-			fmt.Println("Invalid line:", line)
-			continue
+	// Goroutine for checking user input
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			fmt.Print(">> ")
+			text, _ := reader.ReadString('\n')
+			if strings.TrimSpace(text) == "CONFIG" {
+				configTrigger <- true
+			}
 		}
+	}()
 
-		node := Node{
-			Name: parts[0],
-			IP:   parts[1],
-			Port: parts[2],
+	// Goroutine for accepting connections
+	go func() {
+		for {
+			conn, err := l.Accept()
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Println("Connection accepted")
+
+			// Get IP and port of the connection
+			remoteAddr := conn.RemoteAddr().String()
+			parts := strings.Split(remoteAddr, ":")
+			remoteIP := parts[0]
+			remotePort := parts[1]
+			node := Node{
+				IP:   remoteIP,
+				Port: remotePort,
+			}
+
+			nodes = append(nodes, node)
+			connectionTrigger <- true
 		}
-
-		// Skip if the node is the current node
-        if node.IP == localParts[0] && node.Port == localParts[1]{
-            fmt.Println("Skipping self node", node.Name)
-            nodes = append(nodes, node)
-            continue
-        }
-
-		// Initiate a connection with the node
-		conn, err := net.Dial("tcp", node.IP+":"+node.Port)
-		if err != nil {
-			fmt.Println("Error connecting to node:", err)
-			return
-		}
-		defer conn.Close()
-
-		// Store the connection and save the node
-		node.Connection = conn
-
-		nodes = append(nodes, node)
-
-		fmt.Println("Successfully connected to node", node.Name)
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Error reading file:", err)
-	}
+	}()
 
 	for {
-
+		select {
+		case <-configTrigger:
+			go handleConfiguration()
+		case <-connectionTrigger:
+			go handleConfiguration()
+		}
 	}
 
 	return
